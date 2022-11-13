eea3f6c14ae23575d05e0afa9ea1ce5c62ce9135
minhuits <a41417144@gmail.com>
2022-11-14 04:38:59 +0900

    :bug: CERTIFICATE_VERIFY_FAILED (해결)
    - baseURL https ->  http로 변경

--- lib/src/sources/aip_sources.dart
+++ lib/src/sources/aip_sources.dart
@@ -3,4 +3,3 @@ export 'fct_api.dart';
 export 'fct_version_api.dart';
 export 'super_fct_api.dart';
 export 'super_nct_api.dart';
-export 'weather_http_overrides.dart';
--- lib/src/sources/client.dart
+++ lib/src/sources/client.dart
@@ -3,7 +3,7 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class ApiClient {
   static const _baseURL =
-      'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0';
+      'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0';
 
   static Dio createDio({required bool isLog, PrettyDioLogger? customLogger}) {
     var dio = Dio(BaseOptions(baseUrl: _baseURL));
