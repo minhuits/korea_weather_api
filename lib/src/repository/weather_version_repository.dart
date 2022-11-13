6a81cfc1038920f7e4b8aa5f5396c11011366aa1
minhuits <a41417144@gmail.com>
2022-11-13 22:23:23 +0900

    :sparkles: repository 추가

---
+++ lib/src/repository/weather_version_repository.dart
@@ -0,0 +1,13 @@
+import 'package:xml/xml.dart';
+
+import '../models/request/requests.dart';
+
+abstract class VersionRepository<T1, T2> {
+  Future<T2> getJSON(WeatherVersion weatherVersion);
+  Future<T1> getItemXML(WeatherVersion version, int index);
+  Future<T1> getItemJSON(WeatherVersion version, int index);
+  Future<List<T1>> getItemListJSON(WeatherVersion version);
+  Future<List<T1>> getItemListXML(WeatherVersion version);
+
+  T1 toXml(XmlElement e);
+}
