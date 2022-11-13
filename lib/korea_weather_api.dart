ff1a88e4aea4bbf40306f4e4660eda1ed11acad6
minhuits <a41417144@gmail.com>
2022-11-13 22:32:26 +0900

    :sparkles: library export 추가

--- lib/korea_weather_api.dart
+++ lib/korea_weather_api.dart
@@ -1,7 +1,8 @@
 library korea_weather_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/adapter/adapter.dart';
+export 'src/interface/interface.dart';
+export 'src/models/models.dart';
+export 'src/repository/repository.dart';
+export 'src/sources/aip_sources.dart';
+export 'src/usecase/usecase.dart';
--- test/src/dio_fct.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('dio Fct', () {
-    test('fct Json', () {});
-    test('fct XML', () {});
-  });
-}
