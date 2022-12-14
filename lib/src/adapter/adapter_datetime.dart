d88f32fe5f62818e7e2e45de616a597a57294a0c
minhuits <a41417144@gmail.com>
2022-12-14 23:54:04 +0900

    :recycle: Code Recycle

--- lib/src/adapter/adapter_datetime.dart
+++ lib/src/adapter/adapter_datetime.dart
@@ -1,16 +1,14 @@
 class DateTimeAdapter {
   /// 새로운 new dateTime 생성
-  DateTime nowDate({
-    int? day,
-    int? hour,
-    int? minute,
-  }) {
+  DateTime nowDate({int? day, int? hour, int? minute}) {
+    final nowDate = DateTime.now();
+
     return DateTime(
-      DateTime.now().year,
-      DateTime.now().month,
-      day ?? DateTime.now().day,
-      hour ?? DateTime.now().hour,
-      minute ?? DateTime.now().minute,
+      nowDate.year,
+      nowDate.month,
+      day ?? nowDate.day,
+      hour ?? nowDate.hour,
+      minute ?? nowDate.minute,
     );
   }
 
@@ -18,12 +16,14 @@ class DateTimeAdapter {
   DateTime getSuperFctDate(DateTime now) {
     // 45분 이전이면 현재 시보다 1시간 전 `base_time`을 요청한다.
     if (now.minute <= 45) {
+      // 단, 00:45이면 `baseDate`는 전날이고 `baseTime`은 23:30이다.
       if (now.hour == 0) {
-        // 단. 00:45이면 `base_date`는 전날이고 `base_time`은 23:30이다.
         return nowDate(day: now.day - 1, hour: 23, minute: 30);
       }
+
       return nowDate(hour: now.hour - 1, minute: 30);
     }
+
     //45분 이후면 현재 시와 같은 `base_time`을 요청한다.
     return nowDate(minute: 30);
   }
@@ -32,41 +32,39 @@ class DateTimeAdapter {
   DateTime getSuperNctDate(DateTime now) {
     // 40분 이전이면 현재 시보다 1시간 전 `base_time`을 요청한다.
     if (now.minute <= 40) {
-      if (now.hour == 0) {
-        // 단. 0시 40분 이면 `base_date`는 전날이고 `base_time`은 23:00이다.
-        return nowDate(day: now.day - 1, hour: 23, minute: 00);
-      }
+      // 단. 0시 40분 이면 `baseDate`는 전날이고 `baseTime`은 23:00이다.
+      if (now.hour == 0) return _hour23(now);
+
       return nowDate(hour: now.hour - 1, minute: 00);
     }
+
     //40분 이후면 현재 시와 같은 `base_time`을 요청한다.
     return nowDate(minute: 00);
   }
 
   /// 단기 예보 DateTime
   DateTime getFctDate(DateTime now) {
-    if (shortNowIs(now, 2)) {
-      return nowDate(day: now.day - 1, hour: 23, minute: 00);
-    } else if (shortNowIs(now, 5)) {
-      return nowDate(hour: 02, minute: 00);
-    } else if (shortNowIs(now, 8)) {
-      return nowDate(hour: 05, minute: 00);
-    } else if (shortNowIs(now, 11)) {
-      return nowDate(hour: 08, minute: 00);
-    } else if (shortNowIs(now, 14)) {
-      return nowDate(hour: 11, minute: 00);
-    } else if (shortNowIs(now, 17)) {
-      return nowDate(hour: 14, minute: 00);
-    } else if (shortNowIs(now, 20)) {
-      return nowDate(hour: 17, minute: 00);
-    } else if (shortNowIs(now, 23)) {
-      return nowDate(hour: 20, minute: 00);
-    } else {
-      return nowDate(hour: 23, minute: 00);
-    }
+    final fctList = [
+      _hour23(now),
+      _hour(2),
+      _hour(5),
+      _hour(8),
+      _hour(11),
+      _hour(14),
+      _hour(17),
+      _hour(20),
+    ];
+
+    if (now.hour == 23) return _hour(23);
+
+    return fctList[_dateHour(now)];
   }
 
-  ///(a-3)시 11분 ~ a시 10분 사이 예보
-  bool shortNowIs(DateTime now, int a) {
-    return now.hour < a || (now.hour == a && now.minute <= 10);
+  int _dateHour(DateTime now) => (((now.hour + 1) % 24) / 3).floor();
+
+  DateTime _hour23(DateTime now) {
+    return nowDate(day: now.day - 1, hour: 23, minute: 00);
   }
+
+  DateTime _hour(int hour) => nowDate(hour: hour, minute: 00);
 }
--- lib/src/adapter/adapter_map.dart
+++ lib/src/adapter/adapter_map.dart
@@ -2,21 +2,33 @@ import 'dart:math';
 
 /// 동네 좌표 변환
 class MapAdapter {
+  /// [x] : 경도 (초/100)
   final int x;
+
+  /// [y] : 위도 (초/100)
   final int y;
 
-  MapAdapter(this.x, this.y);
+  const MapAdapter(this.x, this.y);
 
   /// 위경도 (x, y)  -> 동네 좌표(X, Y) 변환
   ///
-  /// x : 경도 (초/100) -> 격자 X
-  /// y : 위도 (초/100) -> 격자 Y
+  /// [x] : 경도 (초/100) -> 격자 X
+  ///
+  /// [y] : 위도 (초/100) -> 격자 Y
+  ///
   ///
   /// ```dart
-  /// // 서울특별시
-  /// final map = Coordinate.changeMap(126.98000833333333, 37.56356944444444);
-  /// print('결과 x = ${map.x}, y = ${map.y}');
-  /// [결과] x = 60, y = 127
+  ///
+  /// // 서울특별시 위도
+  /// const double x = 126.98000833333333;
+  /// const double y = 37.56356944444444;
+  ///
+  /// final map = MapAdapter.changeMap(x, y);
+  ///
+  /// print('서울특별시 x = ${map.x}, y = ${map.y}');
+  ///
+  /// [출력 결과] 서울특별시 x = 60, y = 127
+  ///
   /// ```
   factory MapAdapter.changeMap(double longitude, double latitude) {
     late double deGrad;
@@ -30,17 +42,7 @@ class MapAdapter {
     late double ra;
     late double theta;
 
-    final map = ChangeMapParameter(
-      re: 6371.00877,
-      grid: 5.0,
-      slat1: 30.0,
-      slat2: 60.0,
-      oLon: 126.0,
-      oLat: 38.0,
-      nx: 210 / 5.0,
-      ny: 675 / 5.0,
-      first: 0,
-    );
+    const map = ChangeMapParameter();
 
     if (map.first == 0) {
       late double oLat;
@@ -115,16 +117,16 @@ class ChangeMapParameter {
   ///시작여부
   final int first;
 
-  ChangeMapParameter({
-    this.re,
-    this.grid,
-    this.slat1,
-    this.slat2,
-    this.oLon,
-    this.oLat,
-    this.nx,
-    this.ny,
-    required this.first,
+  const ChangeMapParameter({
+    this.re = 6371.00877,
+    this.grid = 5.0,
+    this.slat1 = 30.0,
+    this.slat2 = 60.0,
+    this.oLon = 126.0,
+    this.oLat = 38.0,
+    this.nx = 210 / 5.0,
+    this.ny = 675 / 5.0,
+    this.first = 0,
   });
 
   ChangeMapParameter copyWith({int? first}) {
